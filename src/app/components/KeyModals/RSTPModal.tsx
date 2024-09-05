4a97a31ee5e7b42390b319bcc9a48a4947f5ab30
haseebrehmanpc <37938986+haseebrehmanpc@users.noreply.github.com>
2024-09-05 17:20:45 +0500

    first commit

---
+++ src/app/components/KeyModals/RSTPModal.tsx
@@ -0,0 +1,92 @@
+import { useEffect, useState } from "react";
+import Modal from "react-modal";
+import RstpUrlKeyInput from "../Inputs/RstpKeyInput";
+import { SubmitRstpButton } from "../SubmitButtons/SubmitRstp";
+
+interface IMessage {
+  message: string;
+  color: string;
+}
+
+interface IRstpModalProps {
+  isOpen: boolean;
+  setOpen: Function;
+}
+
+export const RSTPModal: React.FC<IRstpModalProps> = ({
+  isOpen,
+  setOpen,
+}) => {
+  const [rtspUrl, setRtspUrl] = useState<string>("");
+  const [minerKey, setMinerKey] = useState<string>("");
+  const [message, updateMessage] = useState<IMessage>({
+    message: "",
+    color: "white",
+  });
+  const [disappear, setDisappear] = useState<boolean>(false);
+
+  useEffect(() => {
+    if (isOpen) {
+      document.body.style.overflow = 'hidden';
+    } else {
+      document.body.style.overflow = 'auto';
+    }
+
+    return () => {
+      document.body.style.overflow = 'auto';
+    };
+  }, [isOpen]);
+
+  const handleCloseModal = () => {
+    setOpen(false);
+    setRtspUrl("");
+    setMinerKey("");
+    updateMessage({ message: "", color: "white" });
+  };
+
+  return (
+    <Modal
+      isOpen={isOpen}
+      onRequestClose={handleCloseModal}
+      className="bg-[#0CA7E5] absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-white p-4 w-auto sm:w-[600px] rounded-[10px]"
+      overlayClassName="fixed inset-0 bg-black/60"
+    >
+      <div className="flex justify-end">
+        <button
+          className="text-[20px] rounded-[50%] border-white"
+          onClick={handleCloseModal}
+        >
+          X
+        </button>
+      </div>
+      <div className="flex flex-col justify-center items-center">
+        <h1 className="text-[25px] mb-4">
+          Please enter Ecowitt Rstp Url:
+        </h1>
+        <RstpUrlKeyInput
+          token={rtspUrl}
+          setToken={setRtspUrl}
+          inputType="text"
+          placeholder="Enter Rtsp Url"
+        />
+        <RstpUrlKeyInput
+          token={minerKey}
+          setToken={setMinerKey}
+          inputType="id"
+          placeholder="Enter Miner Key"
+        />
+
+        <SubmitRstpButton
+          rtspUrl={rtspUrl}
+          minerKey={minerKey}
+          updateMessage={updateMessage}
+          disappearInput={setDisappear}
+        />
+
+        <p className={`text-${message.color} text-center text-[17px] mt-10 font-bold`}>
+          {message.message}
+        </p>
+      </div>
+    </Modal>
+  );
+}
---
+++ src/app/components/SubmitButtons/SubmitRstp.tsx
@@ -0,0 +1,60 @@
+import { submitRtspUrl } from "@/app/server/Rtsp";
+import { useWallet } from "@txnlab/use-wallet";
+
+export function SubmitRstpButton({
+  valid,
+  rtspUrl,
+  minerKey,
+  updateMessage,
+  disappearInput,
+}: {
+  valid?: boolean;
+  rtspUrl: string;
+  minerKey: string;
+  updateMessage: ({
+    message,
+    color,
+  }: {
+    message: string;
+    color: string;
+  }) => void;
+  disappearInput: Function;
+}) {
+  const { activeAddress } = useWallet();
+
+  const handleGmcSubmit = async (
+    
+    updateMessage: Function,
+    disappearInput: Function,
+    activeAddress: string
+  ) => {
+    disappearInput(true);
+    updateMessage({ message: "Submitting Key...", color: "white" });
+
+    try {
+      const response = await submitRtspUrl(rtspUrl,minerKey,activeAddress);
+      updateMessage(response?.data);
+    } catch (error) {
+      console.error("Error submitting Gmc key:", error);
+      updateMessage({
+        message: "Error submitting key.",
+        color: "red",
+      });
+    } finally {
+      disappearInput(false);
+    }
+  };
+
+  return (
+    <button
+      onClick={() =>
+        handleGmcSubmit( updateMessage,disappearInput,activeAddress!)
+      }
+      className='py-4 px-6 text-base font-medium rounded-lg focus:outline-none bg-[#00FFFF] cursor-pointer first-letter'
+        // ${ isValidKeys ? "bg-[#00FFFF] cursor-pointer" : "bg-gray-400 cursor-not-allowed"}
+      // disabled={!isValidKeys}
+    >
+      Submit
+    </button>
+  );
+}
